ec6b1dd709107c3dca8b97ad636fc5d6d23bb184
absiddik676 <absiddik8200@gmail.com>
2023-06-10 01:08:31 +0600

    selected class data send in database done

--- src/pages/Classes/SingleClass/SingleClass.jsx
+++ src/pages/Classes/SingleClass/SingleClass.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../provider/AuthProvider';
+import axios from 'axios';
 
 const SingleClass = ({singleClass}) => {
+    const {user} = useContext(AuthContext)
+    const handelSelectClass = (classes)=>{
+
+        const selectedClass = {classId:classes._id,name:classes.name,price:classes.price,studentEmail:user?.email,image:classes.image}
+
+        axios.post(`${import.meta.env.VITE_mainURL}/selected-class`,selectedClass)
+        .then(res =>{
+            console.log(res.data);
+        })
+    }
     return (
         <div>
                 <div className="card w-96 bg-base-100 ">
@@ -14,13 +26,13 @@ const SingleClass = ({singleClass}) => {
                             </div>
                             <div className="card-actions justify-end">
                                 <button
+                                onClick={()=>handelSelectClass(singleClass)}
                                     type="button"
                                     className="px-3 py-2 mt-7 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex"
                                 >
                                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
                                     </svg>
-
                                     <span className="ml-2">Select this class</span>
                                 </button>
                             </div>
--- src/pages/Classes/classes/Classes.jsx
+++ src/pages/Classes/classes/Classes.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import React, { useState } from 'react';
 import SingleClass from '../SingleClass/SingleClass';
 
@@ -18,9 +17,7 @@ const Classes = () => {
     return (
         <div className='max-w-7xl grid grid-cols-3 gap-5 mx-auto pt-24'>
             {
-                classes.map(singleClass => <SingleClass key={singleClass._id} singleClass={singleClass}/>)
-                
-                
+                classes.map(singleClass => <SingleClass key={singleClass._id} singleClass={singleClass}/>)   
             }
         </div>
     );
