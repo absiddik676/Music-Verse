f9d57401e828fec7c5a35b1131e21283f58673a4
absiddik676 <absiddik8200@gmail.com>
2023-06-09 18:01:23 +0600

    classes page added and add some animataion in banner page

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import SignUp from "../pages/SignUp/SignUp";
 import Login from "../pages/Login/Login";
+import Classes from "../pages/Classes/Classes";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         {
             path:'login',
             element:<Login/>
+        },
+        {
+            path:'classes',
+            element:<Classes/>
         },
       ]
     },
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,10 +3,11 @@ import img from '../../../assets/img/home_hero_01.png'
 import bgImg from '../../../assets/img/wave.svg'
 import Img1 from '../../../assets/img/floating_notes_01.png'
 import Img2 from '../../../assets/img/floating_notes_02.png'
+import BouncingMusicNode from '../../../component/AnimatedImage';
 
 const Banner = () => {
     return (
-        <div style={{ backgroundImage: `url(${bgImg})` }} className='bg-[#f8d9e8] bg-no-repeat bg-bottom  mb-60'>
+        <div style={{ backgroundImage: `url(${bgImg})` }} className='bg-[#f8d9e8] bg-no-repeat bg-bottom pt-10 mb-60'>
             <div className='mx-auto max-w-7xl bg-no-repeat relative pt-24 flex items-center justify-evenly'>
                 <div className='w-1/2 pb-32'>
                     <h3 className='text-2xl font-bold text-red-500'>ARE YOU LOOKING FOR</h3>
@@ -17,10 +18,10 @@ const Banner = () => {
                     <img src={img} alt="" />
                 </div>
                 <div className='absolute top-24 left-80'>
-                    <img src={Img1} alt="" />
+                <BouncingMusicNode translateY1={'-5px'} translateY2={'10px'} img={Img1}/>
                 </div>
                 <div className='absolute top-24'>
-                    <img src={Img2} alt="" />
+                <BouncingMusicNode translateY1={'10px'} translateY2={'15px'} img={Img2} />
                 </div>
             </div>
         </div>
