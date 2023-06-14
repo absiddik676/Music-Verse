3a564463dd09458f25d6bdfe36dfc43fbfca2673
absiddik676 <absiddik8200@gmail.com>
2023-06-14 12:50:54 +0600

    active  link  added

--- src/pages/Home/Navbar/Navbar.jsx
+++ src/pages/Home/Navbar/Navbar.jsx
@@ -1,93 +1,150 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import { FiLogOut, FiLogIn } from 'react-icons/fi';
 import useAdmin from '../../../hook/useAdmin';
 import useInstructor from '../../../hook/useInstructor';
 import useStudent from '../../../hook/useStudent';
 import { AuthContext } from '../../../Provider/AuthProvider';
+import ActiveRoute from '../../../Routers/ActiveRoute';
 const Navbar = () => {
-    const { user, logOut, loading } = useContext(AuthContext);
-    const [isStudent,setIsStudent] = useState();
-    const handleLogout = () => {
-        logOut()
-            .then(() => { })
-            .catch(error => {
-                console.log(error);
-            })
-    }
-    useEffect(()=>{
-        fetch(`${import.meta.env.VITE_mainURL}/user/student/${user?.email}`)
-        .then(res => res.json())
-        .then(data=> {
-            setIsStudent(data)
-        })
-    },[user])
-    const [isAdmin,isAdminLoading] = useAdmin();
-    const [isInstructor,isInstructorLoading] = useInstructor();
-    
-    console.log(isStudent);
-    return (
-        <div className='max-w-7xl mx-auto '>
-            <div className="navbar max-w-7xl bg-transparent z-50 fixed ">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/'>Home</Link></li>
-                            <li><Link to='/instructor'>Instructors</Link></li>
-                            <li><Link to='/classes'>Classes</Link></li>
-                            {user?<li><Link>Dashboard </Link></li>:''}
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/instructor'>Instructors</Link></li>
-                        <li><Link to='/classes'>Classes</Link></li>
-                        {
-                            loading ? ' ' : <>{user?<li><Link to={`/dashboard/${isAdmin && 'ManageUser' || isInstructor && 'myClasses' || isStudent && 'selected'}`}>Dashboard </Link></li>:''}</>
-                        }
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ? <>
-                            <button
-                                className="flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg shadow hover:bg-red-600"
-                                onClick={handleLogout}
-                            >
-                                <FiLogOut />
-                                <span>Logout</span>
-                            </button>
-                        </> : <>
-                            <Link to='/login'>
-                                <button
-                                    className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
+  const { user, logOut, loading } = useContext(AuthContext);
+  const [isStudent, setIsStudent] = useState();
+  const [navbarBg, setNavbarBg] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setNavbarBg(true);
+      } else {
+        setNavbarBg(false);
+      }
+    };
 
-                                >
-                                    <FiLogIn />
-                                    <span>Login</span>
-                                </button>
-                            </Link>
-                        </>
-                    }
+    window.addEventListener('scroll', handleScroll);
 
-                    {user ? <>
-                        <div className="avatar ml-6">
-                            <div className=" rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 w-12">
-                                <img src={user?.photoURL} />
-                            </div>
-                        </div>
-                    </> : ''}
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const handleLogout = () => {
+    logOut()
+      .then(() => { })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_mainURL}/user/student/${user?.email}`)
+      .then(res => res.json())
+      .then(data => {
+        setIsStudent(data)
+      })
+  }, [user])
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const [isInstructor, isInstructorLoading] = useInstructor();
+
+  console.log(isStudent);
+  return (
+    <div className=''>
+      <div className={`navbar mr-55 bg-transparent z-50 fixed ${navbarBg ? 'bg-gray-900 text-white ' : 'bg-transparent'}`}>
+        <div className="navbar-start md:pl-20">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className=" menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><ActiveRoute to='/'>Home</ActiveRoute></li>
+              <li><ActiveRoute to='/instructor'>Instructors</ActiveRoute></li>
+              <li><ActiveRoute to='/classes'>Classes</ActiveRoute></li>
+              {
+                loading ? ' ' : <>{user ? <li><Link to={`/dashboard/${isAdmin && 'ManageUser' || isInstructor && 'myClasses' || isStudent && 'selected'}`}>Dashboard </Link></li> : ''}</>
+              }
+               <div className='flex items-center justify-end'>
+            {
+              user ? <>
+                <button
+                  className="flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg shadow hover:bg-red-600"
+                  onClick={handleLogout}
+                >
+                  <FiLogOut />
+                  <span>Logout</span>
+                </button>
+              </> : <>
+                <Link to='/login'>
+                  <button
+                    className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
+
+                  >
+                    <FiLogIn />
+                    <span>Login</span>
+                  </button>
+                </Link>
+              </>
+            }
+
+            {user ? <>
+              <div className="avatar ml-6">
+                <div className=" rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 w-12">
+                  <img src={user?.photoURL} />
                 </div>
-            </div>
+              </div>
+            </> : ''}
+          </div>
+            </ul>
+           
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="gap-6 menu-horizontal px-1">
+            <li><ActiveRoute to='/'>Home</ActiveRoute></li>
+            <li><ActiveRoute to='/instructor'>Instructors</ActiveRoute></li>
+            <li><ActiveRoute to='/classes'>Classes</ActiveRoute></li>
+            {
+              loading ? ' ' : <>{user ? <li><ActiveRoute to={`/dashboard/${isAdmin && 'ManageUser' || isInstructor && 'myClasses' || isStudent && 'selected'}`}>Dashboard </ActiveRoute></li> : ''}</>
+            }
+          
+          </ul>
+        </div>
+        <div className="navbar-end hidden md:block md:pr-20">
+          <div className='flex items-center justify-end'>
+            {
+              user ? <>
+                <button
+                  className="flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg shadow hover:bg-red-600"
+                  onClick={handleLogout}
+                >
+                  <FiLogOut />
+                  <span>Logout</span>
+                </button>
+              </> : <>
+                <Link to='/login'>
+                  <button
+                    className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
+
+                  >
+                    <FiLogIn />
+                    <span>Login</span>
+                  </button>
+                </Link>
+              </>
+            }
+
+            {user ? <>
+              <div className="avatar ml-6">
+                <div className=" rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 w-12">
+                  <img src={user?.photoURL} />
+                </div>
+              </div>
+            </> : ''}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Navbar;
